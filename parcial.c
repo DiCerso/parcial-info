2cafcaf85140af0c8e083fdf06b3bb96eb77c11f
DiCerso <lautaro_cer@hotmail.com>
2022-09-27 14:48:08 -0300

    reentrega

--- parcial.c
+++ parcial.c
@@ -11,9 +11,32 @@ int main(int argc, char *argv[])
     char DNI[20] = "";
     char ERROR[255] = "== ERROR ==";
 
-    char dato = argv[1][0];
+    int confirm = 0;
 
-    int confirm = cuit(dato, argv[2], DNI);
+    if (argc == 1)
+    { // si no se ingresa nada
+        confirm = 3;
+    }
+    else if (argc == 2)
+    { // si se ingresa solo el tipo de persona
+        char dato = argv[1][0];
+
+        if (validar_tipo(dato) == 2 || validar_tipo(dato) == 1)
+        { // en caso de que el tipo este bien solo salta error de dni
+            confirm = 2;
+        }
+        else
+        { // en caso contrario saltan ambos
+            confirm = 3;
+        }
+    }
+    else if (argc == 3)
+    { // en caos de estar todos los datos ingresados correctamente sigue a la confirmacion
+        char dato = argv[1][0];
+
+        confirm = cuit(dato, argv[2], DNI);
+    }
+
     switch (confirm)
     {
     case 1:
@@ -29,6 +52,7 @@ int main(int argc, char *argv[])
 
     default:
         printf("%s", DNI);
+
         break;
     }
 
@@ -37,143 +61,147 @@ int main(int argc, char *argv[])
 
 int cuit(char t, char *dni, char *cuit)
 {
+
     int error = 0;
     int error2 = 0;
-    switch (validar_tipo(t))
+
+    switch (validar_tipo(t)) // llamamos a validar_tipo para verificar si es hombre o mujer
     {
     case 2:
-        strcpy(cuit, "20-");
+        strcpy(cuit, "20-"); // en caso de ser hombre
         break;
     case 1:
-        strcpy(cuit, "27-");
+        strcpy(cuit, "27-"); // en caso de ser mujer
         break;
 
     default:
-        error = 1;
+        error = 1; // en caso de error, colocamos error = 1
         break;
     }
-    strcat(cuit, dni);
 
-    if (validar_dni(cuit) == 1)
+    if (validar_dni(dni) == 1) // llamamos a dni para verificar si esta bien
     {
-        error2 = 1;
+        error2 = 1; // en caso de que no, colocamos error2 = 1
     }
-    if (error == 1 && error2 == 1)
+    if (error == 1 && error2 == 1) // si esta mal el tipo de persona como dni devolvemos un 3
     {
         return 3;
     }
-    else if (error2 == 1)
+    else if (error2 == 1) // si solo esta mal el dni devolvemos un 2
     {
         return 2;
     }
+    else if (error == 1)
+    { // si solo esta mal la persona juridica devuelve un 1
+        return 1;
+    }
 
-    return error;
+    if (error2 == 0 && error == 0) // en caso de que no surgieran errores
+    {
+        strcat(cuit, dni); // concatenamos el dni al cuit
+        int sum = 0;
+        int multi = 5;
+        for (int i = 0; i < 11; i++) // hacemos un for recorriendo 11 posiciones del cuit contando el '-'
+        {
+            if (*cuit != '-') // en caso de que no sea '-'
+            {
+                int num = *cuit - '0'; // restamos al char del cuit el char '0' para que me quede el valor ASCII de forma numerica
+                sum += (num * multi);  // realizamos la cuenta
+                if (multi == 2)        // si llego a 2 multi vuelve a 7
+                {
+                    multi = 7;
+                }
+                else // en caso de que no llegue se sigue restando
+                {
+                    multi--;
+                }
+            }
+            cuit++;
+        }
+        int result = (sum * 10) / 11;
+        result = 11 - (result) % 10; // 20-87654321 suma = 160,     (160/11)%10 = 5 ejemplo
+        char dato = result + '0';    // sumamos el numero a '0' para conseguir el numero en ASCII
+        int bandera = 0;
+        char dato2 = ' '; // inicializamos dato2
+        switch (dato)     // dato == '9' + 1  (:)      dato == '9' + 2  (;)
+        {
+        case ':': // si dato es : lo guardamos en 2 char para separar el numero en 10
+            dato = '1';
+            dato2 = '0';
+            break;
+        case ';': // si dato es ; lo guardamos en 2 char para separar el numero en 11
+            dato = '1';
+            dato2 = '1';
+            break;
+        default:
+            break;
+        }
+        if (dato2 != ' ') // si es : o ;
+        {
+            *cuit = '-';
+            cuit++;
+            *cuit = dato;
+            cuit++;
+            *cuit = dato2; // sumamos ambos char al string del cuit
+        }
+        else
+        {
+            *cuit = '-';
+            cuit++;
+            *cuit = dato; // sumamos el numero al tring del cuit
+            cuit++;
+        }
+    }
+
+    return error; // devuelvo el error
+}
+
+int validar_dni(char *dni)
+{
+    if (dni)
+    {
+        int cuenta = 0;
+        while (*dni) // mientras exista char en dni
+        {
+
+            if (cuenta > 8) // en caso de que supere los 8 char devuelve error
+            {
+                return 1;
+            }
+            if (*dni < '0' || *dni > '9') // verifica que sean numeros
+            {
+                return 1;
+            }
+            dni++;
+            cuenta++;
+        }
+        if (cuenta != 8) // en caso de ser distinto de 8 devuelve error
+        {
+            return 1;
+        }
+        else
+        {
+            return 0; // en caso de que no surgiera ningun error devuelve 0
+        }
+    }
+    else
+    {
+        return 1;
+    }
 }
 
 int validar_tipo(char tipo)
 {
-    if (tipo == 'M' || tipo == 'm')
+    if (tipo == 'M' || tipo == 'm') // si es hombre devuelve 2
     {
         return 2;
     }
-    else if (tipo == 'F' || tipo == 'f')
+    else if (tipo == 'F' || tipo == 'f') // si es mujer devuelve 1
     {
         return 1;
     }
     else
     {
-        return 0;
+        return 0; // si no es ninguno devuelve error
     }
-}
-
-int validar_dni(char *dni)
-{
-
-    int multi = 5;
-    float sum = 0;
-    int flag = 0;
-    for (int i = 0; i < 11; i++)
-    {
-        if (*dni != '-')
-        {
-            int num = *dni - '0';
-            if (*dni < '0' || *dni > '9')
-            {
-                if (!*dni)
-                {
-                    if (flag == 1)
-                    {
-                        return 1;
-                    }
-                    return 0;
-                }
-                return 1;
-            }
-            sum += (num * multi);
-            if (multi == 2)
-            {
-                multi = 7;
-            }
-            else
-            {
-                multi--;
-            }
-        }
-        else if (*dni == '-')
-        {
-            flag = 1;
-        }
-        dni++;
-    }
-    int result = (sum / 11) * 10;
-    result = 11 - (result) % 10; // 20-87654321 suma = 160,     (160/11)%10 = 5 ejemplo
-    char dato = result + '0';
-    int bandera = 0;
-    char dato2 = ' ';
-    switch (dato) // dato == '9' + 1  (:)      dato == '9' + 2  (;)
-    {
-    case ':':
-        dato = '1';
-        dato2 = '0';
-        break;
-    case ';':
-        dato = '1';
-        dato2 = '1';
-        break;
-    default:
-        break;
-    }
-    while (bandera != 1)
-    {
-        if (!*dni)
-        {
-            if (bandera == 2)
-            {
-                *dni = dato;
-                bandera = 1;
-            }
-            else
-            {
-                if (bandera == 0)
-                {
-                    *dni = '-';
-                    if (dato2 != ' ')
-                    {
-                        bandera = 3;
-                    }
-                    else
-                    {
-                        bandera = 2;
-                    }
-                }
-                else if (bandera == 3)
-                {
-                    *dni = dato2;
-                    bandera = 2;
-                }
-            }
-        }
-        dni++;
-    }
-    return 0;
 }
